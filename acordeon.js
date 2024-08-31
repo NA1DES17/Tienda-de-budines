9a3b43fa715ad8500ee04e0cbc95d4a6488ff20b
Nicolas Prom <nosoyna1des17@gmail.com>
2024-08-31 20:28:47 -0300

    Falta video(s) y descripción inicial

--- acordeon.js
+++
@@ -1,7 +0,0 @@
-const acordeon = document.getElementsByClassName("contenedor-acordeon");
-
-for (i = 0; i < acordeon.length; i++) {
-  acordeon[i].addEventListener("click", function () {
-    this.classList.toggle("activa");
-  });
-}
---
+++ scriptIndex.js
@@ -0,0 +1,42 @@
+const acordeon = document.getElementsByClassName("contenedor-acordeon");
+
+for (i = 0; i < acordeon.length; i++) {
+  acordeon[i].addEventListener("click", function () {
+    this.classList.toggle("activa");
+  });
+}
+
+// --------------Sticky header---------------
+window.addEventListener("scroll", function () {
+  var header = this.document.querySelector("header");
+  var main = this.document.querySelector("main");
+  header.classList.toggle("abajo", this.window.scrollY > 0);
+  main.classList.toggle("abajo", this.window.scrollY > 0);
+});
+// --------------Sticky header---------------
+
+let slideIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".opinion");
+  const slider = document.querySelector(".slider");
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = totalSlides - 1;
+  } else {
+    slideIndex = index;
+  }
+
+  const offset = -slideIndex * 100;
+  slider.style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(step) {
+  showSlide(slideIndex + step);
+}
+
+// Muestra el primer slide
+showSlide(slideIndex);
